2b580b36e06edb4275bd004f4604b57c9fc3191c
mtiosejo <mtiosejo@gmail.com>
2018-10-31 21:28:26 -0700

    Implemented client-side GET request

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    let propertyId = Number(window.location.pathname.replace(/\//,''));
+    if (propertyId > 0) {
+      $.get(`/reservations/${propertyId}`, result => {
+        console.log('This is a test');
+        this.setState({data: result})
+      })
+    } else {
+      $.get('/reservations', result => {
+        this.setState({data: result[0]})
+      }, 'json')
+    }
   }
 
   render () {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <div>Hello World!</div>
     )
   }
 }
