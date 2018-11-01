8b1854fe403faa7d15a7dbc43785fb8891675757
mtiosejo <mtiosejo@gmail.com>
2018-10-31 21:30:22 -0700

    Implemented server-side GET request

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const Reservations = require('../db/index.js');
-let app = express();
+const app = express();
+const port = 3001;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/../client.dist'));
+app.use(express.static(__dirname + '/../client/dist'));
 
-Reservations.Reservations.create({
-  _id: 1,
-  reservation: 'YE BOI!'
-}, () => {});
+app.get('/:reservations', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/dist/index.html'));
+});
 
-let port = 3000;
+app.get('/reservations/:propertyId', (req, res) => {
+  Reservations.find({propertyId: req.params.propertyId}, (err, data) => {
+    err ? console.error(err) : res.status(200).send(data);
+  })
+});
 
 app.listen(port, () => console.log(`listening on port ${port}`));
