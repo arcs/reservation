2b642a0f6e41c7aacff92e776bae5ab2eb94c20a
mtiosejo <mtiosejo@gmail.com>
2018-11-23 21:09:53 -0800

    Updated schema to reflect db changes

--- db/index.js
+++ db/index.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; // this code solves a deprication warning for Mongoose mPromise
-mongoose.connect('mongodb://localhost/reservation', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/reservation', { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Mongo DB is running');
-});
+db.once('open', () => console.log('Mongo DB is running'));
 
 const reservationSchema = mongoose.Schema({
-  propertyId: Number,
-  costPerNight: Number,
-  reviewCount: Number,
-  availableDate: Number,
-  reservedDate: Number
+  propertyid: Number,
+  costpernight: Number,
+  reviewcount: Number,
+  reserveddate: [Number],
+  starrating: String
 });
 
 const Reservation = mongoose.model('Reservation', reservationSchema);
 
-module.exports = Reservation;
+module.exports.Reservation = Reservation;
+module.exports.db = db;
