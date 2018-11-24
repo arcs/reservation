6a02cbb1209556cc261de2d786d6e1e7a86e973d
mtiosejo <mtiosejo@gmail.com>
2018-11-23 21:08:06 -0800

    Separated out react.dom render statement

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,34 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
+import Reservation from './components/Reservation.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: []
-    };
-  }
-
-  componentDidMount() {
-    let propertyId = Number(window.location.pathname.replace(/\//,''));
-    if (propertyId > 0) {
-      $.get(`/reservations/${propertyId}`, result => {
-        console.log('This is a test');
-        this.setState({data: result})
-      })
-    } else {
-      $.get('/reservations', result => {
-        this.setState({data: result[0]})
-      }, 'json')
-    }
-  }
-
-  render () {
-    return (
-      <div>Hello World!</div>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Reservation />, document.getElementById('reservation'));
