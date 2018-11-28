4a2882eab2b89a810d278f90e5a3f617c78a7dd5
mtiosejo <mtiosejo@gmail.com>
2018-11-28 02:16:11 -0800

    Updated connection address to Atlas

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; // this code solves a deprication warning for Mongoose mPromise
-mongoose.connect('mongodb://localhost:27017/reservation', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost:27017/reservation', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://mtiosejo:Philippe829!@fec1-iino1.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
