70f6dec64bdfa57a022c3b80d38b9541455aec95
mtiosejo <mtiosejo@gmail.com>
2018-11-29 00:57:20 -0800

    Cleaned up syntactical issues

--- client/src/components/Reservation.jsx
+++ client/src/components/Reservation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import ReactStars from 'react-stars'
+import ReactStars from 'react-stars';
 import $ from 'jquery';
 
 const MainContainer = styled.div`
